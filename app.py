cf11787437ed054afdc3a767bb967c4202f7d194
kiran-A <kiranaglawe18@gmail.com>
2026-04-11 14:01:41 +0530

    adding up .env and making some changes

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+# from  spark import total_income,total_expenditure,total_balance,today_income,today_expenditure
 from flask import Flask, render_template,redirect, request, url_for
+from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import date
 
 import os
@@ -6,39 +8,22 @@ import csv
 
 app = Flask(__name__)
 
-def calculation():
-    if os.path.exists("data/data.csv"):
-        with open("data/data.csv", newline="") as file:
-            reader = csv.DictReader(file)
-            data = list(reader)
-    total_income = 0
-    total_expenditure = 0
-    total_balance = 0
-    for i in data:
-        if i['Type']=='Income':
-            total_income=total_income+int(i['Amount'])
-        elif i['Type']=='Expense':
-            total_expenditure=total_expenditure+int(i['Amount'])
-        else:
-            pass
-    total_balance=total_income-total_expenditure
-    return total_income,total_balance,total_expenditure
-
-def result():
-    total_income,total_balance,total_expenditure=calculation()
-    return total_income,total_expenditure,total_balance
-
 @app.route("/")
 def home():
-    total_income,total_expenditure,total_balance=result()
-    name="Kiran"
-    data=[]
-    if os.path.exists("data/data.csv"):
-        with open("data/data.csv", newline="") as file:
-            reader = csv.DictReader(file)
-            data = list(reader)
-    data=data[-5:]
-    return render_template("index.html",name=name,data=data,total_income=total_income,total_expenditure=total_expenditure,total_balance=total_balance)
+    return render_template("login.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@app.route("/signup_save",methods=["POST"])
+def signup_save():
+    if len(request.form["pass"])>=8:
+        password = generate_password_hash(request.form["pass"])
+        name = request.form["name"]
+        email = request.form["email"]
+        return redirect(url_for("home"))
+
 
 @app.route("/submit", methods=["POST"])
 def submit():
@@ -54,7 +39,6 @@ def submit():
         if not file_exists:
             writer.writerow(["Date","Amount", "Type", "Note"])
         writer.writerow([today,int(money),entry_type,note])
-    calculation()
     return redirect(url_for("home"))
 
 if __name__ == "__main__":
